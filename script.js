bd26e2b44732f4024aeb965284b7892a2d98b3e3
maks <a@a.a>
2020-11-28 11:36:16 +0300

    slider

---
+++ script.js
@@ -0,0 +1,23 @@
+const prevArrow = document.querySelector('.prev-arrow');
+const nextArrow = document.querySelector('.next-arrow');
+const cards = document.querySelector('.cards');
+
+let current = 0;
+let width = window.getComputedStyle(cards);
+
+width = parseInt(width.width);
+const cardList = document.querySelectorAll('.cards__img');
+const handleNextClick = (e)=>{
+    
+    const next = (current + 1) % (cardList.length / 3);
+    
+    cardList.forEach( (e)=>{
+        console.log( `${-width * next}px`);
+        
+        e.style.left =  `${-width * next}px`
+    }   );
+
+    current = next;
+
+}
+nextArrow.addEventListener('click', handleNextClick)
