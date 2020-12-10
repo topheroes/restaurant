798d6da829aebcb777c884f7922ecfc8939121c4
Pavel <tr@tr.tr>
2020-12-10 15:37:25 +0300

    static

--- tests.js
+++ tests.js
@@ -1,15 +1,31 @@
 const { chromium } = require('playwright');
+var static = require('node-static');
+const { exit } = require('process');
+
+var fileServer = new static.Server();
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        fileServer.serve(request, response);
+        
+    }).resume();
+}).listen(80);
 
 (async ()=>{
     const browser = await chromium.launch({headless:false});
 
     const context = await browser.newContext({
-        viewport: { width: 2560, height: 1440 }
+        viewport: { width: 3440, height: 1440 }
     });
     const page = await context.newPage();
-    await page.goto("index.html")
-    await page.screenshot({path: `screenshot_2560.png`});
-
+    await page.goto("http://localhost/index.html")
+    await page.screenshot({path: `screenshot_3440.png`});
+    await browser.close();
+    exit();
 })()
+
 
 
