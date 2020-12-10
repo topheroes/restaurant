a1d6f44ba651faf6e2ad5aafd16877ada054df30
Pavel <tr@tr.tr>
2020-12-10 16:12:10 +0300

    localhost

--- tests.js
+++ tests.js
@@ -2,17 +2,17 @@ const { chromium } = require('playwright');
 var static = require('node-static');
 const { exit } = require('process');
 
-// var fileServer = new static.Server();
+var fileServer = new static.Server();
 
-// require('http').createServer(function (request, response) {
-//     request.addListener('end', function () {
-//         //
-//         // Serve files!
-//         //
-//         fileServer.serve(request, response);
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        fileServer.serve(request, response);
         
-//     }).resume();
-// }).listen(80);
+    }).resume();
+}).listen(80);
 
 (async ()=>{
     const browser = await chromium.launch();
@@ -21,7 +21,7 @@ const { exit } = require('process');
         viewport: { width: 3440, height: 1440 }
     });
     const page = await context.newPage();
-    await page.goto("https://google.com")
+    await page.goto("http://localhost/index.html")
     await page.screenshot({path: `screenshot_3440.png`});
     await browser.close();
     
